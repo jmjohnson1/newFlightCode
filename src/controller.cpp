c46ea0b09948b74370d3a1b60168ebe9e333d357
jmjohnson1 <106408381+jmjohnson1@users.noreply.github.com>
2024-01-27 10:23:27 -0600

    Implemented PID position controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -2,137 +2,173 @@
 #include "Arduino.h"
 
 // Angle attitude controller from the original dRehmflight code //
-AngleAttitudeController::AngleAttitudeController(const float (&Kp)[3], const float (&Ki)[3], const float (&Kd)[3], float iLimit) {
-	for (int i = 0; i < 3; i++) {
-		Kp_[i] = Kp[i];
-		Ki_[i] = Ki[i];
-		Kd_[i] = Kd[i];
-	}
-	iLimit_ = iLimit;
+AngleAttitudeController::AngleAttitudeController(const float (&Kp)[3],
+                                                 const float (&Ki)[3],
+                                                 const float (&Kd)[3],
+                                                 float iLimit) {
+  for (int i = 0; i < 3; i++) {
+    Kp_[i] = Kp[i];
+    Ki_[i] = Ki[i];
+    Kd_[i] = Kd[i];
+  }
+  iLimit_ = iLimit;
 }
 
-void AngleAttitudeController::Update(const float (&setpoints)[3], const Attitude &att, const float (&gyroRates)[3], float dt, bool noIntegral) {
-	rollPID_ = AnglePID(setpoints[0], att.roll, gyroRates[0], dt, noIntegral, ROLL);	
-	pitchPID_ = AnglePID(setpoints[1], att.pitch, gyroRates[1], dt, noIntegral, PITCH);	
-	yawPID_ = RatePID(setpoints[2], gyroRates[2], dt, noIntegral, YAW);
+void AngleAttitudeController::Update(const float (&setpoints)[3],
+                                     const Attitude &att,
+                                     const float (&gyroRates)[3], float dt,
+                                     bool noIntegral) {
+  rollPID_ =
+      AnglePID(setpoints[0], att.roll, gyroRates[0], dt, noIntegral, ROLL);
+  pitchPID_ =
+      AnglePID(setpoints[1], att.pitch, gyroRates[1], dt, noIntegral, PITCH);
+  yawPID_ = RatePID(setpoints[2], gyroRates[2], dt, noIntegral, YAW);
 }
 
-void AngleAttitudeController::GetMotorCommands(float motorCommandsNormalized[4], float throttleSetting) {
-	//motorCommandsNormalized[0] = throttleSetting - pitchPID_ + rollPID_ + yawPID_;
-	//motorCommandsNormalized[1] = throttleSetting - pitchPID_ - rollPID_ - yawPID_;
-	//motorCommandsNormalized[2] = throttleSetting + pitchPID_ - rollPID_ + yawPID_;
-	//motorCommandsNormalized[3] = throttleSetting + pitchPID_ + rollPID_ - yawPID_;
-	motorCommandsNormalized[0] = throttleSetting + pitchPID_ + rollPID_ - yawPID_;
-	motorCommandsNormalized[1] = throttleSetting + pitchPID_ - rollPID_ + yawPID_;
-	motorCommandsNormalized[2] = throttleSetting - pitchPID_ - rollPID_ - yawPID_;
-	motorCommandsNormalized[3] = throttleSetting - pitchPID_ + rollPID_ + yawPID_;
+void AngleAttitudeController::GetMotorCommands(float motorCommandsNormalized[4],
+                                               float throttleSetting) {
+  motorCommandsNormalized[0] = throttleSetting + pitchPID_ + rollPID_ - yawPID_;
+  motorCommandsNormalized[1] = throttleSetting + pitchPID_ - rollPID_ + yawPID_;
+  motorCommandsNormalized[2] = throttleSetting - pitchPID_ - rollPID_ - yawPID_;
+  motorCommandsNormalized[3] = throttleSetting - pitchPID_ + rollPID_ + yawPID_;
 }
 
-float AngleAttitudeController::AnglePID(float setpoint, float measuredAngle, float gyroRate, float dt, bool noIntegral, AxisToControl axis) {
-	float *integral_prev = nullptr;
-	float Kp = Kp_[axis];
-	float Ki = Ki_[axis];
-	float Kd = Kd_[axis];
-	switch (axis) {
-		case ROLL:
-			integral_prev = &prevIntegralRoll_;
-			break;
-		case PITCH:
-			integral_prev = &prevIntegralPitch_;
-			break;
-		default:
-			return 0.0f;
-	}
-	float error = setpoint - measuredAngle;
+float AngleAttitudeController::AnglePID(float setpoint, float measuredAngle,
+                                        float gyroRate, float dt,
+                                        bool noIntegral, AxisToControl axis) {
+  float *integral_prev = nullptr;
+  float Kp = Kp_[axis];
+  float Ki = Ki_[axis];
+  float Kd = Kd_[axis];
+  switch (axis) {
+  case ROLL:
+    integral_prev = &prevIntegralRoll_;
+    break;
+  case PITCH:
+    integral_prev = &prevIntegralPitch_;
+    break;
+  default:
+    return 0.0f;
+  }
+  float error = setpoint - measuredAngle;
   float integral = *integral_prev + error * dt;
   if (noIntegral) { // Don't let integrator build if this is set
     integral = 0.0f;
   }
-  integral = constrain(integral, -iLimit_, iLimit_); // Saturate integrator to prevent unsafe buildup
-  float derivative = -gyroRate; // This is an approximation of the error derivative
-  float PIDOutput = 0.01 * (Kp*error + Ki*integral + Kd*derivative);
-	*integral_prev = integral;
+  integral =
+      constrain(integral, -iLimit_,
+                iLimit_); // Saturate integrator to prevent unsafe buildup
+  float derivative =
+      -gyroRate; // This is an approximation of the error derivative
+  float PIDOutput = 0.01 * (Kp * error + Ki * integral + Kd * derivative);
+  *integral_prev = integral;
 
-	return PIDOutput;
+  return PIDOutput;
 }
 
-float AngleAttitudeController::RatePID(float setpoint, float measuredRate, float dt, bool noIntegral, AxisToControl axis) {
-	float *integral_prev = nullptr;
-	float *error_prev = nullptr;
-	float Kp = Kp_[axis];
-	float Ki = Ki_[axis];
-	float Kd = Kd_[axis];
-	switch (axis) {
-		case YAW:
-			integral_prev = &prevIntegralYaw_;
-			error_prev = &prevErrorYaw_;
-			break;
-		default:
-			return 0.0f;
-	}
-	float error = setpoint - measuredRate;
-	float integral = *integral_prev + error*dt;
-	if (noIntegral) {
-		integral = 0.0f;
-	}
-	integral = constrain(integral, -iLimit_, iLimit_);
-	float derivative = (error - *error_prev)/dt;
-	float PIDOutput = 0.01f * (Kp*error + Ki*integral + Kd*derivative);
-	*integral_prev = integral;
-	*error_prev = error;
-	return PIDOutput;
+float AngleAttitudeController::RatePID(float setpoint, float measuredRate,
+                                       float dt, bool noIntegral,
+                                       AxisToControl axis) {
+  float *integral_prev = nullptr;
+  float *error_prev = nullptr;
+  float Kp = Kp_[axis];
+  float Ki = Ki_[axis];
+  float Kd = Kd_[axis];
+  switch (axis) {
+  case YAW:
+    integral_prev = &prevIntegralYaw_;
+    error_prev = &prevErrorYaw_;
+    break;
+  default:
+    return 0.0f;
+  }
+  float error = setpoint - measuredRate;
+  float integral = *integral_prev + error * dt;
+  if (noIntegral) {
+    integral = 0.0f;
+  }
+  integral = constrain(integral, -iLimit_, iLimit_);
+  float derivative = (error - *error_prev) / dt;
+  float PIDOutput = 0.01f * (Kp * error + Ki * integral + Kd * derivative);
+  *integral_prev = integral;
+  *error_prev = error;
+  return PIDOutput;
 }
 
-PositionController::PositionController(const float (&Kp)[3], const float (&Ki)[3], const float (&Kd)[3], float iLimit = 25.0f) {
-	for (int i = 0; i < 3; i++) {
-		Kp_[i] = Kp[i];
-		Ki_[i] = Ki[i];
-		Kd_[i] = Kd[i];
-	}
-	iLimit_ = iLimit;
+PositionController::PositionController(const float (&Kp)[3],
+                                       const float (&Ki)[3],
+                                       const float (&Kd)[3], float iLimit) {
+  // Initialize the gain matrices
+  Kp_ = Eigen::Matrix3f::Zero();
+  Ki_ = Eigen::Matrix3f::Zero();
+  Kd_ = Eigen::Matrix3f::Zero();
+
+  // Set the diagonals to the supplied gains
+  for (int i = 0; i < 3; i++) {
+    Kp_.diagonal()[i] = Kp[i];
+    Ki_.diagonal()[i] = Ki[i];
+    Kd_.diagonal()[i] = Kd[i];
+  }
+  iLimit_ = iLimit;
+  prevIntegral_ << 0.0, 0.0, 0.0;
+  prevError_ << 0.0, 0.0, 0.0;
 }
 
-void PositionController::Update(const Eigen::Vector3f &posSetpoints, const Eigen::Vector3f &currentPosition, const Attitude &att, float dt, bool noIntegral, float maxAngle) {
-	Eigen::Vector3f posError_ned, integral, derivative, desAcc_ned;
-	posError_ned = posSetpoints - currentPosition;
-	integral = prevIntegral_ + posError_ned*dt;
-	// Prevent integral from building up if the thrust is low (noIntegral passed
-	// as true).
-	if (noIntegral) {
-		integral = integral*0;
-	}
-	// Prevent integral windup
-	for (int i = 1; i < 3; i++) {
-		integral[i] = constrain(integral[i], -iLimit_, iLimit_);
-	}
-	derivative = (posError_ned - prevError_)/dt;
+void PositionController::Update(const Eigen::Vector3f &posSetpoints,
+                                const Eigen::Vector3f &currentPosition,
+                                const Attitude &att, float dt, bool noIntegral,
+                                float maxAngle) {
+  // Declare some variables
+  Eigen::Vector3f posError_ned, integral, derivative, desAcc_ned;
+  float desAcc_b3; // Desired thrust resolved in the 3 axis of the body frame
+  float Cy = cos(att.yaw * DEG_TO_RAD);
+  float Sy = sin(att.yaw * DEG_TO_RAD);
+  float sinRoll, sinPitch; // Sin of the roll and pitch angles
+  float maxAngle_sinArg = sin(maxAngle * DEG_TO_RAD);
 
-	// Calculate desired acceleration in the NED frame using PID
-	desAcc_ned = Kp_*posError_ned + Ki_*integral + Kd_*derivative;
+  posError_ned = posSetpoints - currentPosition;
+  integral = prevIntegral_ + posError_ned * dt;
+  // Prevent integral from building up if the thrust is low (noIntegral passed
+  // as true).
+  if (noIntegral) {
+    integral = integral * 0;
+  }
+  // Prevent integral windup
+  for (int i = 1; i < 3; i++) {
+    integral[i] = constrain(integral[i], -iLimit_, iLimit_);
+  }
+  derivative = (posError_ned - prevError_) / dt;
 
-	// Extract the third term and use it to get the desired thrust in the body
-	// frame. Need to add the amount of thrust required to hover with the
-	// current attitude.
-	float desAcc_b3 = desAcc_ned[2] - 9.81/cos(att.roll)/cos(att.pitch);
-	desiredThrust_ = QUAD_MASS*(desAcc_b3);
+  // Calculate desired acceleration in the NED frame using PID
+  desAcc_ned = Kp_ * posError_ned + Ki_ * integral + Kd_ * derivative;
 
-	float yaw_rad = att.yaw*DEG_TO_RAD;
-	float Cy = cos(yaw_rad);
-	float Sy = sin(yaw_rad);
-	
-	// Calculate a new desired attitude based on the amount of thrust available
-	// and the desired accelerations in n1 and n2;
+  // Extract the third term and use it to get the desired thrust in the body
+  // frame. Need to add the amount of thrust required to hover with the
+  // current attitude.
+  desAcc_b3 = desAcc_ned[2] - 9.81 / cos(att.roll) / cos(att.pitch);
+  desiredThrust_ = QUAD_MASS * (desAcc_b3);
+  desiredThrust_ = constrain(desiredThrust_, MIN_THRUST, MAX_THRUST);
+  desAcc_b3 =
+      desiredThrust_ / QUAD_MASS; // It's easier to constrain the thrust. maybe
 
-	// Let's make sure we can't break arcsin. This also guarantees that the
-	// argument of the square root is positive.
-	if () {
+  // Calculate a new desired attitude based on the amount of thrust available
+  // and the desired accelerations in n1 and n2;
+  // Let's make sure we can't break arcsin.
+  if (abs(desiredThrust_ - MIN_THRUST) > EPSILON) {
+    sinRoll = (Sy * desAcc_ned[0] - Cy * desAcc_ned[1]) / desAcc_b3;
+    sinRoll = constrain(sinRoll, -maxAngle_sinArg, maxAngle_sinArg);
+    desiredRoll_ = asin(sinRoll);
 
-	}
+    sinPitch = (Cy * desAcc_ned[0] + Sy * desAcc_ned[1]) / desAcc_b3 /
+               sqrt(1 - sinRoll * sinRoll);
+    sinPitch = constrain(sinPitch, -maxAngle_sinArg, maxAngle_sinArg);
+    desiredPitch_ = asin(sinPitch);
 
-
-	float sinRoll = (Sy*desAcc_ned[0] - Cy*desAcc_ned[1])/desAcc_b3;
-	float sinPitch = (Cy*desAcc_ned[0] + Sy*desAcc_ned[1])/desAcc_b3/sqrt(1 - sinRoll*sinRoll);
-
-	desiredRoll_ = asin(sinRoll);
-	desiredPitch_ = asin(sinPitch);
+		desiredRoll_ *= RAD_TO_DEG;
+		desiredPitch_ *= RAD_TO_DEG;
+  } else {
+    // If the thrust is small, please don't try to use it.
+    desiredRoll_ = 0.0f;
+    desiredPitch = 0.0f;
+  }
 }
--- src/controller.h
+++ src/controller.h
@@ -1,116 +1,136 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#include "eigen.h"
 #include "commonDefinitions.h"
-
+#include "eigen.h"
 
 class AngleAttitudeController {
 public:
-	AngleAttitudeController(const float (&Kp)[3], const float (&Ki)[3], const float (&Kd)[3], float iLimit = 25.0f);
-	void Update(const float (&setpoints)[3], const Attitude &att, const float (&gyroRates)[3], float dt, bool noIntegral);
-	
-	void GetMotorCommands(float motorCommandsNormalized[4], float throttleSetting);
+  AngleAttitudeController(const float (&Kp)[3], const float (&Ki)[3],
+                          const float (&Kd)[3], float iLimit = 25.0f);
+  void Update(const float (&setpoints)[3], const Attitude &att,
+              const float (&gyroRates)[3], float dt, bool noIntegral);
 
-	float GetRollPID() {return rollPID_;}
-	float GetPitchPID() {return pitchPID_;}
-	float GetYawPID() {return yawPID_;}
+  void GetMotorCommands(float motorCommandsNormalized[4],
+                        float throttleSetting);
 
-	// Need to modify a pointer to an outside array to give out the controller constants
-	void GetKp(float KpOut[3]) {
-		KpOut[0] = Kp_[0];
-		KpOut[1] = Kp_[1];
-		KpOut[2] = Kp_[2];
-	}
-	void GetKi(float KiOut[3]) {
-		KiOut[0] = Ki_[0];
-		KiOut[1] = Ki_[1];
-		KiOut[2] = Ki_[2];
-	}
-	void GetKd(float KdOut[3]) {
-		KdOut[0] = Kd_[0];
-		KdOut[1] = Kd_[1];
-		KdOut[2] = Kd_[2];
-	}
+  float GetRollPID() { return rollPID_; }
+  float GetPitchPID() { return pitchPID_; }
+  float GetYawPID() { return yawPID_; }
 
-	void SetKp(const float (&KpIn)[3]) {
-		Kp_[0] = KpIn[0];
-		Kp_[1] = KpIn[1];
-		Kp_[2] = KpIn[2];
-	}
-	void SetKi(const float (&KiIn)[3]) {
-		Ki_[0] = KiIn[0];
-		Ki_[1] = KiIn[1];
-		Ki_[2] = KiIn[2];
-	}
-	void SetKd(const float (&KdIn)[3]) {
-		Kd_[0] = KdIn[0];
-		Kd_[1] = KdIn[1];
-		Kd_[2] = KdIn[2];
-	}
+  // Need to modify a pointer to an outside array to give out the controller
+  // constants
+  void GetKp(float KpOut[3]) {
+    KpOut[0] = Kp_[0];
+    KpOut[1] = Kp_[1];
+    KpOut[2] = Kp_[2];
+  }
+  void GetKi(float KiOut[3]) {
+    KiOut[0] = Ki_[0];
+    KiOut[1] = Ki_[1];
+    KiOut[2] = Ki_[2];
+  }
+  void GetKd(float KdOut[3]) {
+    KdOut[0] = Kd_[0];
+    KdOut[1] = Kd_[1];
+    KdOut[2] = Kd_[2];
+  }
+
+  void SetKp(const float (&KpIn)[3]) {
+    Kp_[0] = KpIn[0];
+    Kp_[1] = KpIn[1];
+    Kp_[2] = KpIn[2];
+  }
+  void SetKi(const float (&KiIn)[3]) {
+    Ki_[0] = KiIn[0];
+    Ki_[1] = KiIn[1];
+    Ki_[2] = KiIn[2];
+  }
+  void SetKd(const float (&KdIn)[3]) {
+    Kd_[0] = KdIn[0];
+    Kd_[1] = KdIn[1];
+    Kd_[2] = KdIn[2];
+  }
 
 private:
-	enum AxisToControl {
-		ROLL = 0,
-		PITCH = 1,
-		YAW = 2,
-	};
-	float AnglePID(float setpoint, float measuredAngle, float gyroRate, float dt, bool noIntegral, AxisToControl axis);
-	float RatePID(float setpoint, float measuredRate, float dt, bool noIntegral, AxisToControl axis);
+  enum AxisToControl {
+    ROLL = 0,
+    PITCH = 1,
+    YAW = 2,
+  };
+  float AnglePID(float setpoint, float measuredAngle, float gyroRate, float dt,
+                 bool noIntegral, AxisToControl axis);
+  float RatePID(float setpoint, float measuredRate, float dt, bool noIntegral,
+                AxisToControl axis);
 
-	float rollPID_  = 0.0f;
-	float pitchPID_ = 0.0f;
-	float yawPID_   = 0.0f;
+  float rollPID_ = 0.0f;
+  float pitchPID_ = 0.0f;
+  float yawPID_ = 0.0f;
 
-	float prevIntegralRoll_  = 0.0f;
-	float prevIntegralPitch_ = 0.0f;
-	float prevIntegralYaw_   = 0.0f;
-	float prevErrorYaw_      = 0.0f;
+  float prevIntegralRoll_ = 0.0f;
+  float prevIntegralPitch_ = 0.0f;
+  float prevIntegralYaw_ = 0.0f;
+  float prevErrorYaw_ = 0.0f;
 
-	float Kp_[3];
-	float Ki_[3];
-	float Kd_[3];
+  float Kp_[3];
+  float Ki_[3];
+  float Kd_[3];
 
-	float iLimit_;
+  float iLimit_;
 };
 
 class PositionController {
 public:
-	const float QUAD_MASS = 0.846f;  // Quadcopter mass in Kg
-	const float MAX_THRUST = 8.9f*4.0f; // Maximum total thrust
-	PositionController(const float (&Kp)[3], const float (&Ki)[3], const float (&Kd)[3], float iLimit = 25.0f);
-	void Update(const Eigen::Vector3f &posSetpoints, const Eigen::Vector3f &currentPosition, const Attitude &att, float dt, bool noIntegral, float maxAngle);
+  // TODO: get rid of hardcoding here
+  const float QUAD_MASS = 0.846f;       // Quadcopter mass in Kg
+  const float MAX_THRUST = 8.9f * 4.0f; // Maximum total thrust
+  const float MIN_THRUST = 0.0f;
+  // Used for floating point comparison later. Generally best to avoid.
+  const float EPSILON = 0.001f;
 
-	float GetDesiredThrottle() {
-		// TODO: Get rid of the hardcoding
-		return desiredThrust_/MAX_THRUST;
-	}
-	float GetDesiredRoll() { return desiredRoll_; }
-	float GetDesiredPitch() { return desiredPitch_; }
+  PositionController(const float (&Kp)[3], const float (&Ki)[3],
+                     const float (&Kd)[3], float iLimit = 25.0f);
+  void Update(const Eigen::Vector3f &posSetpoints,
+              const Eigen::Vector3f &currentPosition, const Attitude &att,
+              float dt, bool noIntegral, float maxAngle);
 
-	Eigen::Matrix3f GetKp() { return Kp_; }
-	Eigen::Matrix3f GetKi() { return Ki_; }
-	Eigen::Matrix3f GetKd() { return Kd_; }
+  float GetDesiredThrottle() { return abs(desiredThrust_) / MAX_THRUST; }
+  float GetDesiredRoll() { return desiredRoll_; }
+  float GetDesiredPitch() { return desiredPitch_; }
 
-	void SetKp(const Eigen::Matrix3f &KpIn) { Kp_ = KpIn; }
-	void SetKp(const Eigen::Matrix3f &KpIn) { Kp_ = KpIn; }
-	void SetKp(const Eigen::Matrix3f &KpIn) { Kp_ = KpIn; }
+  Eigen::Matrix3f GetKp() { return Kp_; }
+  Eigen::Matrix3f GetKi() { return Ki_; }
+  Eigen::Matrix3f GetKd() { return Kd_; }
+
+  void SetKp(const float (&KpIn)[3]) {
+    for (int i = 0; i < 3; i++) {
+      Kp_.diagonal()[i] = KpIn[i];
+    }
+  }
+  void SetKi(const float (&KiIn)[3]) {
+    for (int i = 0; i < 3; i++) {
+      Ki_.diagonal()[i] = KiIn[i];
+    }
+  }
+  void SetKd(const float (&KdIn)[3]) {
+    for (int i = 0; i < 3; i++) {
+      Kd_.diagonal()[i] = KdIn[i];
+    }
+  }
 
 private:
+  float desiredThrust_ = 0.0f; // Calculated desired thrust
+  float desiredPitch_ = 0.0f;  // Calculated desired pitch angle (degrees)
+  float desiredRoll_ = 0.0f;   // Calculated desired roll angle (degrees)
 
+  Eigen::Vector3f prevIntegral_; // Save the last integral calculation
+  Eigen::Vector3f prevError_;    // Save the last error calculation
 
-	float desiredThrust_ = 0.0f;
-	float desiredPitch_  = 0.0f;
-	float desiredRoll_   = 0.0f;
+  Eigen::Matrix3f Kp_;
+  Eigen::Matrix3f Ki_;
+  Eigen::Matrix3f Kd_;
 
-	Eigen::Vector3f prevIntegral_(0.0f, 0.0f, 0.0f);
-	Eigen::Vector3f prevError_(0.0f, 0.0f, 0.0f);
-
-	Eigen::Matrix3f Kp_;
-	Eigen::Matrix3f Ki_;
-	Eigen::Matrix3f Kd_;
-
-	float iLimit_; // Maximum value for the integral portion
+  float iLimit_; // Maximum value for the integral portion
 };
 
 #endif
